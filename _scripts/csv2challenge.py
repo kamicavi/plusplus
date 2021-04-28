df97ace9f61c820adb861ef5d2627c16b2e4d059
Ewan Klein <ewan@raw-text.io>
2021-04-28 10:05:41 +0100

    Added argument parsing

--- _scripts/csv2challenge.py
+++ _scripts/csv2challenge.py
@@ -1,42 +1,52 @@
-#!/usr/local/bin/env python3
+#!/usr/bin/env python3
 # coding: utf-8
 
+from slugify import slugify
+import yaml
+import pandas as pd
+import argparse
 import os
 
 from jinja2 import Environment, FileSystemLoader
 
 env = Environment(
-    loader=FileSystemLoader('.'),lstrip_blocks=True, trim_blocks=True)
-import argparse
-
-import pandas as pd
-import yaml
-from slugify import slugify
+    loader=FileSystemLoader('.'), lstrip_blocks=True, trim_blocks=True)
 
 parser = argparse.ArgumentParser()
 
 challenge_path = '../_challenges'
 
+
 def input_csv(infile):
+    """
+    Open a CSV file for reading
+    """
     with open(infile, newline='') as csvfile:
         df = pd.read_csv(csvfile)
     return df
 
+
 def relabel_df(df):
-    df = df.rename(columns={'Title':'title',
-                            'Teaser text':'excerpt',
-                            'Description':'description', 
-                            'Example projects':'projects',
-                            'Tags':'tags',
-                            'Mentors':'mentors',
-                            'Resources':'resources',
-                            'Badge':'badge',
-                            'Level':'level'
-                           })
+    """
+    Modify the column labels to fit into Jekyll. Default to lowercase.
+    """
+    df = df.rename(columns={'Title': 'title',
+                            'Teaser text': 'excerpt',
+                            'Description': 'description',
+                            'Example projects': 'projects',
+                            'Tags': 'tags',
+                            'Mentors': 'mentors',
+                            'Resources': 'resources',
+                            'Badge': 'badge',
+                            'Level': 'level'
+                            })
     return df
 
 
 def csv2dict(df):
+    """
+    Convert the dataframe created by pandas to a dict
+    """
     csvdict = df.to_dict(orient='records')[0]
 
     try:
@@ -56,11 +66,18 @@ def csv2dict(df):
 
 
 def make_outfname(csvdict):
+    """
+    Use the title of the challenge to generate a filename for the output.
+    """
     outfname = slugify(csvdict['title']) + '.md'
     outfpath = os.path.join(challenge_path, outfname)
     return outfpath
 
+
 def build_content(csvdict):
+    """ 
+    Construct a new dictionary that will be used to populate the template.
+    """
     yamlkeys = ['title', 'excerpt', 'tags', 'mentors', 'badge', 'level']
     #yamlkeys = ['title', 'excerpt', 'tags']
     yamldict = {}
@@ -69,13 +86,17 @@ def build_content(csvdict):
     #sidekeys = ['mentors', 'badge', 'level']
 
     for k in yamlkeys:
-        yamldict[k]= csvdict[k]
+        yamldict[k] = csvdict[k]
         del content[k]
     yml = yaml.dump(yamldict)
     content['yaml'] = yml
     return content
 
-def render_template(content, outfpath, tpl = 'template.md', debug = False):
+
+def render_template(content, outfpath, tpl='template.md', debug=False):
+    """
+    Render the template and write to a file.
+    """
     template = env.get_template(tpl)
 
     t = template.render(content=content)
@@ -87,15 +108,16 @@ def render_template(content, outfpath, tpl = 'template.md', debug = False):
 
 
 def main():
-    df = input_csv('tp1.csv')   
-    df = relabel_df(df)
-    csvdict = csv2dict(df)
-    outfpath = make_outfname(csvdict)
-    content = build_content(csvdict)
-    render_template(content, outfpath, debug=False)
+    parser.add_argument(dest='infile', type=str, nargs='+')
+    args = parser.parse_args()
+    for arg in args.infile:
+        df = input_csv(arg)
+        df = relabel_df(df)
+        csvdict = csv2dict(df)
+        outfpath = make_outfname(csvdict)
+        content = build_content(csvdict)
+        render_template(content, outfpath, debug=False)
 
 
 if __name__ == "__main__":
     main()
-
-
